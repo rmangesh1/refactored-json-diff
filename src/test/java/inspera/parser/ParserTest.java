012bfd13e39b69fe636f15d918fb7e74cad60b65
Mangesh <mrana@nets.eu>
2018-06-19 16:41:03 +0200

    parser test setup

--- src/test/java/inspera/parser/ParserTest.java
+++ src/test/java/inspera/parser/ParserTest.java
@@ -1,20 +1,39 @@
 package inspera.parser;
 
-import inspera.parser.ExaminationDiffParser;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import inspera.parser.mapper.ExamDiffObjectMapper;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  *
  */
 public class ParserTest {
 
-    ExaminationDiffParser parser = new ExaminationDiffParser();
+    private ExaminationDiffParser parser = new ExaminationDiffParser();
+
+    ObjectMapper objectMapper = ExamDiffObjectMapper.getObjectMapper();
+
+    private JsonNode beforeJsonNode;
+
+    private JsonNode afterJsonNode;
 
     @Before
-    public void setup() {
-        // TODO Load in test data from before.json and after.json
+    public void setup() throws URISyntaxException, IOException {
+        beforeJsonNode = objectMapper.readTree(new String(Files.readAllBytes(Paths.get(ParserTest.class.getClassLoader().getResource("before.json").toURI()))));
+        afterJsonNode = objectMapper.readTree(new String(Files.readAllBytes(Paths.get(ParserTest.class.getClassLoader().getResource("after.json").toURI()))));
     }
 
     // TODO Define tests here
+    @Test
+    public void test() {
+        System.out.println(beforeJsonNode);
+    }
 
 }
